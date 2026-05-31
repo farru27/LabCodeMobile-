7e65c18627478ad8993ba062a2e698a6b7afe1ca
farru27 <federicotourino318@gmail.com>
2026-05-30 21:00:43 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,166 @@
+let files = {
+
+html:
+`<h1>Hola VSCode 🚀</h1>
+
+<p>Editor Android funcionando</p>`,
+
+css:
+`body{
+font-family:Arial;
+padding:40px;
+background:#f5f5f5;
+}
+
+h1{
+color:#0078d7;
+}`,
+
+js:
+`console.log("VSCode Mobile Android");`
+
+};
+
+let current = "html";
+
+const editor =
+document.getElementById("editor");
+
+editor.value = files[current];
+
+/* CAMBIAR TAB */
+
+function changeTab(tab, element){
+
+files[current] = editor.value;
+
+current = tab;
+
+editor.value = files[current];
+
+document.querySelectorAll(".tab")
+.forEach(t=>{
+t.classList.remove("active");
+});
+
+element.classList.add("active");
+
+}
+
+/* EJECUTAR */
+
+function runCode(){
+
+files[current] = editor.value;
+
+const finalCode = `
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<style>
+
+${files.css}
+
+</style>
+
+</head>
+
+<body>
+
+${files.html}
+
+<script>
+
+${files.js}
+
+<\/script>
+
+</body>
+
+</html>
+
+`;
+
+document.getElementById("preview")
+.srcdoc = finalCode;
+
+}
+
+/* MOSTRAR PREVIEW */
+
+function showPreview(){
+
+runCode();
+
+document.getElementById("editorBox")
+.style.display = "none";
+
+document.getElementById("previewBox")
+.style.display = "flex";
+
+document.getElementById("editorBtn")
+.classList.remove("active");
+
+document.getElementById("previewBtn")
+.classList.add("active");
+
+}
+
+/* VOLVER A EDITOR */
+
+document.getElementById("editorBtn")
+.onclick = function(){
+
+document.getElementById("editorBox")
+.style.display = "flex";
+
+document.getElementById("previewBox")
+.style.display = "none";
+
+document.getElementById("previewBtn")
+.classList.remove("active");
+
+document.getElementById("editorBtn")
+.classList.add("active");
+
+};
+
+/* AUTOGUARDADO */
+
+editor.addEventListener("input", ()=>{
+
+files[current] = editor.value;
+
+localStorage.setItem(
+"vscode-mobile-android",
+JSON.stringify(files)
+);
+
+});
+
+/* CARGAR GUARDADO */
+
+const saved =
+localStorage.getItem(
+"vscode-mobile-android"
+);
+
+if(saved){
+
+files = JSON.parse(saved);
+
+editor.value = files[current];
+
+}
+
+runCode();
+
+/* PRIVACIDAD */
+
 function showPrivacy(){
 
 const modal = document.createElement("div");
